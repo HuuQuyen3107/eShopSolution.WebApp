fc1f231065bf56680cd50334cac762f533657dd0
Quyền Trần <qtran0835@gmail.com>
2022-10-12 00:51:45 +0700

    Setup EshopDbContext

---
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using eShopSolution.Data.Entities;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { set; get; }
+        public DbSet<Category> Categories { set; get; }
+    }
+}
