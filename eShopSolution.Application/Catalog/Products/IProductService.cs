ad1fd34b1008586d0a106ba6eb691ce2f343abb6
Quyền Trần <qtran0835@gmail.com>
2022-10-25 18:10:28 +0700

    Get list user

--- eShopSolution.Application/Catalog/Products/IProductService.cs
+++ eShopSolution.Application/Catalog/Products/IProductService.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace eShopSolution.Application.Catalog.Products
 {
-    public interface IManageProductService
+    public interface IProductService
     {
         Task<int> Create(ProductCreateRequest request);
 
@@ -38,5 +38,7 @@ namespace eShopSolution.Application.Catalog.Products
         Task<ProductImageViewModel> GetImageById(int imageId);
 
         Task<List<ProductImageViewModel>> GetListImages(int productId);
+
+        Task<PageResult<ProductViewModel>> GetAllByCategoryId(string languageId, GetPublicProductPagingRequest request);
     }
 }
